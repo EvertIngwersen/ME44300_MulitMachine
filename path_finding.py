dc53901f93320cfebb45d6ceba0af0a5f474efc3
Evert Ingwersen <everti2000@gmail.com>
2025-04-22 14:03:11 +0200

    added planar pdf

--- path_finding.py
+++ path_finding.py
@@ -11,8 +11,8 @@ import math
 import matplotlib.pyplot as plt
 
 random_seed = random.seed(77)
-n = 15
-m = 10
+n = 10 # amount of nodes
+m = 10 # size of the graph 
 
 x = [random.random()*m for _ in range(n)]
 y = [random.random()*m for _ in range(n)] 
@@ -23,7 +23,7 @@ integer_list = list(range(0, n))
 vertices_dict = {}
 
 for i in range(n):
-    num_elements = random.randint(1, 4)  # Random number between 1 and 4
+    num_elements = random.randint(1, 3)  # Random number between 1 and max_connections
     vertices_dict[i] = random.sample(integer_list, num_elements)
 
 for key in vertices_dict:
@@ -34,15 +34,23 @@ for key in vertices_dict:
     
 plt.figure(1)
 plt.scatter(x, y, marker='o')  
-plt.title('Data Points')
+plt.title('Graph with Connected Nodes')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid(True)
+# Add node indices
 for i in range(n):
-    plt.text(x[i] + 0.01, y[i] + 0.01, str(i), fontsize=9, color='black')
-plt.show()  
-
-
+    plt.text(x[i] + 0.1, y[i] + 0.1, str(i), fontsize=9, color='black')
+# Draw connections
+for node, connections in vertices_dict.items():
+    for neighbor in connections:
+        x_coords = [nodes[node][0], nodes[neighbor][0]]
+        y_coords = [nodes[node][1], nodes[neighbor][1]]
+        plt.plot(x_coords, y_coords, color='gray', linewidth=1)
+# Set fixed axis limits
+plt.xlim(0, m)
+plt.ylim(0, m)
+plt.show()
 
 distance_matrix = np.zeros((n,n))
 for i in range(n):
