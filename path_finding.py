d9ee98fb84a2ff810e68333e5a1f60ae9b6a99f2
Evert Ingwersen <everti2000@gmail.com>
2025-04-22 13:17:34 +0200

    created distance matrix

--- path_finding.py
+++ path_finding.py
@@ -7,6 +7,7 @@ Created on Tue Apr 22 12:56:09 2025
 
 import numpy as np
 import random
+import math
 import matplotlib.pyplot as plt
 
 random_seed = random.seed(77)
@@ -23,6 +24,24 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.grid(True)
 plt.show()  
+
+distance_matrix = np.zeros((n,n))
+
+for i in range(n):
+    for j in range(n):
+        distance_matrix[i,j] = math.sqrt(((x[i]-x[j])**2)+(y[i]-y[j])**2)
+        
+
+        
+
+
+
+
+
+
+
+
+
 
 
 
