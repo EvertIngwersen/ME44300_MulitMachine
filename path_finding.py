3e18eddbe01e8768697013903ce632da92349055
Evert Ingwersen <everti2000@gmail.com>
2025-04-22 13:02:26 +0200

    added random nodes

--- path_finding.py
+++ path_finding.py
@@ -5,3 +5,15 @@ Created on Tue Apr 22 12:56:09 2025
 @author: evert
 """
 
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+random_seed = random.seed(77)
+n = 5
+x = [random.random() for _ in range(n)]
+y = [random.random() for _ in range(n)] 
+
+nodes = np.array([x,y]).T
+
+
