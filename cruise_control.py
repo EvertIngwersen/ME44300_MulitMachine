4d48570354cfeedd7f2c63fec52674adfe7266fd
Evert Ingwersen <everti2000@gmail.com>
2025-04-24 13:46:47 +0200

    made plot of cruise control

--- cruise_control.py
+++ cruise_control.py
@@ -15,11 +15,30 @@ Kp = 500            # proportional gain
 desired_speed = 25 
 
 t_end = 20
-dt = 0.01
+dt = 0.1
 
 t = np.arange(0, t_end, dt)
 
+# initial conditions
+
+v = 0
+u = 0 
+v_history = []
+
+for time in t:
+    u = Kp * (desired_speed - v)
+    dv_dt = (u-b*v) / m
+    v += dv_dt * dt
+    v_history.append(v)
 
 
-
+plt.figure()
+plt.plot(t, v_history, label='Vehicle Speed')
+plt.axhline(desired_speed, color='r', linestyle='--', label='Desired Speed')
+plt.title('Cruise Control System Response')
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m/s)')
+plt.legend()
+plt.grid(True)
+plt.show()
 
