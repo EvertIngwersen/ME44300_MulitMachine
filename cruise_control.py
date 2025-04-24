976edfd5e54be085c57f1ccc1ba4a6750c18db71
Evert Ingwersen <everti2000@gmail.com>
2025-04-24 14:09:23 +0200

    variable desired speed

--- cruise_control.py
+++ cruise_control.py
@@ -13,7 +13,7 @@ b = 50              # damping [Ns/m]
 
 Kp = 500 
 Ki = 1000           # proportional gain
-desired_speed = 25 
+
 
 t_end = 20
 dt = 0.1
@@ -26,21 +26,24 @@ v = 0
 u = 0 
 integral = 0
 v_history = []
+desired_history = []
+
 
 for time in t:
+    desired_speed = 20 + 5 * np.sin(0.5 * time)  # Sine wave: oscillates between 15 and 25
+    desired_history.append(desired_speed)
+
     error = desired_speed - v
     integral += error * dt
     u = Kp * error + Ki * integral
-    
-    dv_dt = (u - b*v) / m
+
+    dv_dt = (u - b * v) / m
     v += dv_dt * dt
     v_history.append(v)
     
-
-
 plt.figure()
 plt.plot(t, v_history, label='Vehicle Speed')
-plt.axhline(desired_speed, color='r', linestyle='--', label='Desired Speed')
+plt.plot(t, desired_history, 'r--', label='Desired Speed')
 plt.title('Cruise Control System Response')
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (m/s)')
