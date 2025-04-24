064ea177d0279f2ba0d76e39e8e2d4b7161f8fbd
Evert Ingwersen <everti2000@gmail.com>
2025-04-24 13:40:42 +0200

    GPT variables

--- cruise_control.py
+++ cruise_control.py
@@ -6,6 +6,20 @@ Created on Thu Apr 24 13:20:44 2025
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-m = 1000 # mass of the car
+m = 1000            # mass of the car [kg]
+b = 50              # damping [Ns/m]
+
+Kp = 500            # proportional gain
+desired_speed = 25 
+
+t_end = 20
+dt = 0.01
+
+t = np.arange(0, t_end, dt)
+
+
+
+
 
