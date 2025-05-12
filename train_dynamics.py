8485cecc687d1afbd6868031728837ad5847f507
Evert Ingwersen <everti2000@gmail.com>
2025-05-12 12:15:08 +0200

    more input param

--- train_dynamics.py
+++ train_dynamics.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-# System Parameters
+# System parameters
 M = 50000       #train mass
 b = 1000        #rolling resistance 
 c = 50          #aero drag coef
@@ -25,6 +25,14 @@ P_min = 0
 P_max = 5e6
 
 # Cost weights
+Q_v = 1         #speed tracking
+R = 1e-7        #power usage
 
+# Reference speed
+v_ref = 10      #reference speed
+
+# Initial state
+s_now = 0
+v_now = 0.5
 
 
