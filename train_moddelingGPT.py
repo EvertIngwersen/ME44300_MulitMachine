d1a0624d7cadebac01d28c65319da47c3ba43810
Evert Ingwersen <everti2000@gmail.com>
2025-05-12 10:08:13 +0200

    added literture

---
+++ train_moddelingGPT.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 10:07:37 2025
+
+@author: evert
+"""
+
---
+++ waterHW.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  6 11:59:43 2025
+
+@author: evert
+"""
+
+import numpy as np
+
+h_ref = 1
+D = 2
+β_max = 4
+H = 3.5
+T_s = 1
+
+u_0 = 2
+
+x = np.arange(0,10,1) 
